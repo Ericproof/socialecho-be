6b6493853c187b1bcde3590fca54b0f8d8ddf05b
eric <583079439@qq.com>
2024-07-31 17:04:47 +1000

    update

--- dist/utils/confirmationToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var crypto = require("crypto");
-var generateConfirmationToken = function generateConfirmationToken(userId) {
-  var currentTime = Date.now().toString();
-  var tokenData = "".concat(currentTime).concat(userId);
-  return crypto.createHash("sha256").update(tokenData).digest("hex");
-};
-module.exports = generateConfirmationToken;
